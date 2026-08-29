db283015e67b4d64145d86e97064e11aa6845433
GolamAzamShakil <golamazam1010@gmail.com>
2026-08-29 18:29:48 +0600

    Keep alive fixes v1

--- src/main.ts
+++ src/main.ts
@@ -111,8 +111,9 @@ async function bootstrap() {
 
   const server = app.getHttpServer();
   server.keepAliveTimeout = 120 * 1000;
-  server.headersTimeout = 120 * 1000;
+  server.headersTimeout = 130 * 1000;
+  server.requestTimeout = 120 * 1000;
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
